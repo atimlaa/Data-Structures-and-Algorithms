299c3d74bee7e5421a5fe4f70556822e4e696fe4
Aryan Timla <54657323+atimlaa@users.noreply.github.com>
2022-03-22 03:02:24 +0545

    anagram .h file

---
+++ Assignment1/csce310hmwrk01prt01.h
@@ -0,0 +1,13 @@
+#ifndef CSCE310HMWRK01PRT01_H
+#define CSCE310HMWRK01PRT01_H
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+bool isAnagram( string , string );
+
+#endif
---
+++ Assignment1/csce310hmwrk01prt02.h
@@ -0,0 +1,14 @@
+#ifndef CSCE310HMWRK01PRT02_H
+#define CSCE310HMWRK01PRT02_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+void printPermutations( string , int );
+
+#endif
